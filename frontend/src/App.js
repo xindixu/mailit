aff25e2ca76dba0c7ae09ea3e5aaf87755d5a059
Xindi Xu <xindixu0@gmail.com>
2021-10-18 15:51:35 -0400

    feat: setup frontend api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // TODO: create a api-fetch module to DRY fetching
+    axios
+      .get("/api/v1/users")
+      .then((resp) => {
+        const { data } = resp;
+        setUsers(data.users);
+      })
+      .catch((error) => console.log(error));
+
+    return () => {};
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {users.map(({ first_name: firstName, last_name: lastName }) => (
+          <li>
+            {firstName} {lastName}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
