03a9c2b50f987cd148211ae383fb663dab1823d5
Xindi Xu <xindixu0@gmail.com>
2021-10-30 15:23:46 -0400

    use antd form with validation

--- frontend/src/components/editor/index.js
+++ frontend/src/components/editor/index.js
@@ -1,23 +1,14 @@
-import React, { useState } from "react"
 import PropTypes from "prop-types"
 import MDEditor from "@uiw/react-md-editor"
 
-const TEMPLATE = `
-Dear {{first_name}}:
-
-I hope that you can take a moment to rest as midterm season continues. Taking five minutes to stretch, breathe or get up for a short walk can make a big difference. Try setting a reminder or alarm on your phone and have a plan in place for what to do with your time to yourself. 
-
-One common method that you might find helpful is the Pomodoro Method. You can use whatever segments of time work best for you, but this image will help you to understand the basic concept.
-
-
-![](https://communications.universitylife.columbia.edu/sites/default/files/civicrm/persist/contribute/images/uploads/static/POMODORO_GRAPHIC_2048x2048_10cc11c5bccfb8239585cd8d58ed7acb.jpg)
-`
 const HEIGHT = 1000
-const MarkdownEditor = () => {
-  const [content, setContent] = useState(TEMPLATE)
-  return <MDEditor value={content} onChange={setContent} height={HEIGHT} />
-}
+const MarkdownEditor = ({ value, onChange }) => (
+  <MDEditor value={value} onChange={onChange} height={HEIGHT} />
+)
 
-MarkdownEditor.propTypes = {}
+MarkdownEditor.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
 
 export default MarkdownEditor
--- frontend/src/pages/templates/index.js
+++ frontend/src/pages/templates/index.js
@@ -1,45 +1,74 @@
 import React, { useState, useCallback } from "react"
-import PropTypes from "prop-types"
-import { Space, Button, Row } from "antd"
+import { Form, Space, Button, Input } from "antd"
 import { SaveOutlined } from "@ant-design/icons"
 import { useHistory } from "react-router-dom"
 import Editor from "../../components/editor"
 
-const Templates = (props) => {
+const TEMPLATE = `
+Dear {{first_name}}:
+
+I hope that you can take a moment to rest as midterm season continues. Taking five minutes to stretch, breathe or get up for a short walk can make a big difference. Try setting a reminder or alarm on your phone and have a plan in place for what to do with your time to yourself. 
+
+One common method that you might find helpful is the Pomodoro Method. You can use whatever segments of time work best for you, but this image will help you to understand the basic concept.
+
+
+![](https://communications.universitylife.columbia.edu/sites/default/files/civicrm/persist/contribute/images/uploads/static/POMODORO_GRAPHIC_2048x2048_10cc11c5bccfb8239585cd8d58ed7acb.jpg)
+`
+
+const Templates = () => {
   const [isSaving, setIsSaving] = useState(false)
+
   const history = useHistory()
 
   const goBack = useCallback(() => {
     history.push("/")
   }, [history])
 
-  const save = useCallback(() => {
-    setIsSaving(true)
+  const onFinish = useCallback(
+    (values) => {
+      setIsSaving(true)
 
-    // FIXME: send request to save the updates
-    setTimeout(() => {
-      setIsSaving(false)
-      goBack()
-    }, 1000)
-  }, [goBack])
+      // FIXME: send request to save the updates
+      setTimeout(() => {
+        setIsSaving(false)
+        goBack()
+      }, 1000)
+    },
+    [goBack]
+  )
 
   return (
-    <Space direction="vertical">
-      <Editor />
-      <Row justify="end">
+    <Form
+      onFinish={onFinish}
+      layout="vertical"
+      initialValues={{ name: "Midterm", content: TEMPLATE }}
+    >
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: "Please enter name!" }]}
+      >
+        <Input placeholder="Midterm template" />
+      </Form.Item>
+      <Form.Item
+        label="Content"
+        name="content"
+        rules={[{ required: true, message: "Please enter content!" }]}
+      >
+        <Editor />
+      </Form.Item>
+      <Form.Item span={24} style={{ textAlign: "right" }}>
         <Space>
           <Button type="default" onClick={goBack}>
             Cancel
           </Button>
-          <Button type="primary" onClick={save} icon={<SaveOutlined />} loading={isSaving}>
-            Save
+          <Button type="primary" htmlType="submit" loading={isSaving} icon={<SaveOutlined />}>
+            Submit
           </Button>
         </Space>
-      </Row>
-    </Space>
+      </Form.Item>
+    </Form>
   )
 }
-
-Templates.propTypes = {}
 
 export default Templates
