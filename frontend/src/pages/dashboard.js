a7744f267007d2dd2040ef2460bcb971adbfa36c
Qianjun Chen <qianjunc@gmail.com>
2021-11-03 14:08:36 -0400

    [front-end] send email and delete campaign feature on dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,20 +1,8 @@
 import { useEffect, useState } from "react"
-import { Table } from "antd"
+import { Card, Col, Row, Table, Popconfirm, Space } from "antd"
+import { Link } from "react-router-dom"
 import apiFetch from "../lib/api-fetch"
 import TemplateIndex from "./templates"
-
-const campaignsTable = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Tag",
-    dataIndex: "tag",
-    key: "tag",
-  },
-]
 
 const getCampaignTableData = (data) =>
   data.map(({ id, attributes: { name, tag, user_id: userId, template_id: templateId } }) => ({
@@ -34,6 +22,41 @@ const Dashboard = () => {
       setCampaigns(data)
     })
   }, [])
+
+  const handleDeleteCampaign = (record) => {
+    apiFetch({ route: `campaigns/${record.id}`, method: "delete" })
+    setCampaigns(campaigns.filter((item) => item.id !== record.id))
+  }
+
+  const campaignsTable = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Tag",
+      dataIndex: "tag",
+      key: "tag",
+    },
+    {
+      title: "operation",
+      dataIndex: "operation",
+      render: (text, record) => (
+        <Space size="middle">
+          <Popconfirm
+            title="Sure to Send?"
+            onConfirm={() => apiFetch({ route: `campaigns/${record.id}/deliver`, method: "post" })}
+          >
+            <a>Send</a>
+          </Popconfirm>
+          <Popconfirm title="Sure to Delete?" onConfirm={() => handleDeleteCampaign(record)}>
+            <a>Delete</a>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ]
 
   return (
     <div>
