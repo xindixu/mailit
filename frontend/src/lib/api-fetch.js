bb73059c2701bf6d53b0ad51be203b70d4441266
Xindi Xu <xindixu0@gmail.com>
2021-11-02 21:15:58 -0400

    feat(dashboard): get data for dashboard

--- frontend/src/lib/api-fetch.js
+++ frontend/src/lib/api-fetch.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
-const apiFetch = ({ route, method = "get" }) => axios.get(`/api/v1/${route}`)
+const apiFetch = ({ route, method = "get" }) =>
+  axios.get(`/api/v1/${route}`).then(({ data, status }) => ({ data: data.data, status }))
 
 export default apiFetch
--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,12 +1,55 @@
-import React from "react"
+import { useEffect, useState } from "react"
 import PropTypes from "prop-types"
+import { Card, Table } from "antd"
+import apiFetch from "../lib/api-fetch"
 
-const Dashboard = () => (
-  <div>
-    <h2>Featured Templates</h2>
-    <h2>Campaigns</h2>
-  </div>
-)
+const campaignsTable = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Tag",
+    dataIndex: "tag",
+    key: "tag",
+  },
+]
+
+const getCampaignTableData = (data) =>
+  data.map(({ id, attributes: { name, tag, user_id: userId, template_id: templateId } }) => ({
+    key: id,
+    id,
+    name,
+    tag,
+    userId,
+    templateId,
+  }))
+
+const Dashboard = () => {
+  const [templates, setTemplates] = useState([])
+  const [campaigns, setCampaigns] = useState([])
+
+  useEffect(() => {
+    apiFetch({ route: "templates" }).then(({ data }) => {
+      setTemplates(data)
+    })
+    apiFetch({ route: "campaigns" }).then(({ data }) => {
+      setCampaigns(data)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h2>Featured Templates</h2>
+      {templates.map(({ id, attributes: { name } }) => (
+        <Card key={id}>{name} </Card>
+      ))}
+      <h2>Campaigns</h2>
+      <Table dataSource={getCampaignTableData(campaigns)} columns={campaignsTable} />
+    </div>
+  )
+}
 
 Dashboard.propTypes = {}
 
