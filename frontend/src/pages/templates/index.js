550bff20bf2e82fa1736533fcb6747699433ae91
Xindi Xu <xindixu0@gmail.com>
2021-11-03 14:33:40 -0400

    fix: update styling

--- frontend/src/pages/templates/index.js
+++ frontend/src/pages/templates/index.js
@@ -2,7 +2,16 @@ import { useEffect, useCallback, useState } from "react"
 import { Modal, Button, Card, Col, Row } from "antd"
 import { Link } from "react-router-dom"
 import { DeleteOutlined, ExclamationCircleOutlined } from "@ant-design/icons"
+import styled from "styled-components"
 import apiFetch from "../../lib/api-fetch"
+import styleSettings from "../../styles"
+
+const { spacer } = styleSettings
+
+// TODO: replace it with a css utility class libraries
+const Wrapper = styled.div`
+  margin-bottom: ${spacer};
+`
 
 const { confirm } = Modal
 const showConfirm = ({ name, onOk, onCancel }) =>
@@ -33,28 +42,28 @@ const TemplateIndex = () => {
   return (
     <>
       <h2>All Templates</h2>
-
-      <Row gutter={16}>
-        {templates.map(({ id, attributes: { name } }) => (
-          <Col key={id} span={6}>
-            <Card>
-              <p>
-                <Link to={`/templates/${id}`}>{name}</Link>
-              </p>
-              <Button
-                shape="circle"
-                danger
-                icon={<DeleteOutlined />}
-                onClick={() => {
-                  showConfirm({ name, onOk: () => onDelete(id) })
-                }}
-                id={`delete template ${name}`}
-              />
-            </Card>
-          </Col>
-        ))}
-      </Row>
-
+      <Wrapper>
+        <Row gutter={16}>
+          {templates.map(({ id, attributes: { name } }) => (
+            <Col key={id} span={6}>
+              <Card>
+                <p>
+                  <Link to={`/templates/${id}`}>{name}</Link>
+                </p>
+                <Button
+                  shape="circle"
+                  danger
+                  icon={<DeleteOutlined />}
+                  onClick={() => {
+                    showConfirm({ name, onOk: () => onDelete(id) })
+                  }}
+                  id={`delete template ${name}`}
+                />
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Wrapper>
       <Button type="primary">
         <Link to="/templates/new">Create A Template</Link>
       </Button>
