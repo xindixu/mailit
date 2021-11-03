f97e86876b253c0c9ec6b871f7de542b3384b9d6
Xindi Xu <xindixu0@gmail.com>
2021-11-03 15:59:54 -0400

    feat: each cucumber test should be independent from each other

--- frontend/features/step-definitions/template-steps.js
+++ frontend/features/step-definitions/template-steps.js
@@ -1,8 +1,32 @@
 const { expect } = require("chai")
-const { When, Then } = require("@cucumber/cucumber")
+const { When, Then, BeforeAll, AfterAll, Before, After } = require("@cucumber/cucumber")
 const { By, until } = require("selenium-webdriver")
 const { removeQuotations, clear } = require("../support")
+const { apiFetch } = require("../api-fetch")
 const { driver } = require("./stepdefs")
+
+let id
+Before(async () => {
+  apiFetch({
+    route: "/templates",
+    method: "post",
+    params: {
+      name: "Merry Christmas",
+      markdown: "Wish you a Merry Christmas",
+      user_id: 1,
+      collaborator_ids: [1],
+    },
+  }).then(({ data }) => {
+    id = data.id
+  })
+})
+
+After(async () => {
+  apiFetch({
+    route: `/templates/${id}`,
+    method: "delete",
+  })
+})
 
 Then(/user can see the template name and content/, async () => {
   // wait until data is fetched
@@ -17,10 +41,10 @@ Then(/user can see the template name and content/, async () => {
 
 When("user updates the template name to {string}", async (value) => {
   await driver.wait(until.elementLocated(By.id("name")))
-
   const nameInput = await driver.findElement(By.id("name"))
-
-  await clear(driver, nameInput)
+  await nameInput.clear()
+  // FIXME: not sure why this won't work with yarn coverage
+  // await clear(driver, nameInput)
   await nameInput.sendKeys(removeQuotations(value))
 })
 
