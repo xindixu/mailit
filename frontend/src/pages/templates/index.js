9f49c24006a87f580fad61859970081046cd804d
Xindi Xu <xindixu0@gmail.com>
2021-11-03 12:55:34 -0400

    feat: delete template with confirm

--- frontend/src/pages/templates/index.js
+++ frontend/src/pages/templates/index.js
@@ -1,13 +1,32 @@
-import { useEffect, useState } from "react"
-import { Card, Col, Row } from "antd"
+import { useEffect, useCallback, useState } from "react"
+import { Modal, Button, Card, Col, Row } from "antd"
 import { Link } from "react-router-dom"
+import { DeleteOutlined, ExclamationCircleOutlined } from "@ant-design/icons"
 import apiFetch from "../../lib/api-fetch"
+
+const { confirm } = Modal
+const showConfirm = ({ name, onOk, onCancel }) =>
+  confirm({
+    title: `Do you Want to delete template "${name}"`,
+    icon: <ExclamationCircleOutlined />,
+    onOk,
+    onCancel,
+  })
 
 const TemplateIndex = () => {
   const [templates, setTemplates] = useState([])
+
   useEffect(() => {
     apiFetch({ route: "templates" }).then(({ data }) => {
       setTemplates(data)
+    })
+  }, [])
+
+  const onDelete = useCallback((id) => {
+    apiFetch({ route: `templates/${id}`, method: "delete" }).then(({ status }) => {
+      if (status === 200) {
+        setTemplates((prevTemplates) => prevTemplates.filter((template) => template.id !== id))
+      }
     })
   }, [])
 
@@ -17,11 +36,19 @@ const TemplateIndex = () => {
       <Row gutter={16}>
         {templates.map(({ id, attributes: { name } }) => (
           <Col key={id} span={6}>
-            <Link to={`/templates/${id}`}>
-              <Card style={{}}>
+            <Card>
+              <Link to={`/templates/${id}`}>
                 <p>{name}</p>
-              </Card>
-            </Link>
+              </Link>
+              <Button
+                shape="circle"
+                danger
+                icon={<DeleteOutlined />}
+                onClick={() => {
+                  showConfirm({ name, onOk: () => onDelete(id) })
+                }}
+              />
+            </Card>
           </Col>
         ))}
       </Row>
