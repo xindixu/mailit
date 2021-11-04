84498853bede87a93e652b60dcd34e815924108d
Xindi Xu <xindixu0@gmail.com>
2021-11-03 20:08:23 -0400

    fix

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "backend_production"
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
