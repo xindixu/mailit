a83ffe25f4178662ebac916d33705496a9a2380f
Qianjun Chen <qianjunc@gmail.com>
2021-11-15 19:27:19 -0500

    [front-end] change the wrong header

--- frontend/src/components/csv/index.js
+++ frontend/src/components/csv/index.js
@@ -22,7 +22,7 @@ const uploadProps = {
   onDrop(e) {
     console.log("Dropped files", e.dataTransfer.files)
   },
-  headers: { Authorization: `Bearer ${sessionStorage.getItem("user_id")}` },
+  headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
 }
 
 const UploadCSV = () => (
