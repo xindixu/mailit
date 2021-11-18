13916cece1edf930e8b146a874629159adee39eb
Xindi Xu <xindixu0@gmail.com>
2021-11-17 20:43:48 -0500

    update rails secret

--- backend/spec/requests/authentication_request_spec.rb
+++ backend/spec/requests/authentication_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'AUTHENTICATION API', type: :request do
         it 'authenticates the user' do 
             @user = User.new(name: 'Test User', email: 'test@example.com', password: 'hello1')
             @user.save
-            expected_token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base, 'HS256')
+            expected_token = JWT.encode({user_id: @user.id}, Rails.application.secret_key_base, 'HS256')
             post '/api/v1/login', params: {email: 'test@example.com', password: 'hello1'}
             expect(response).to have_http_status(:success)
             expect(JSON.parse(response.body)['status']).to eq(200)
