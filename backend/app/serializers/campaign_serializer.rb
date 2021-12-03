357fac72137c9d062df1c88a820575e970fdfa5e
Qianjun Chen <qianjunc@gmail.com>
2021-12-02 20:59:09 -0500

    backend: subject add to serializer

--- backend/app/serializers/campaign_serializer.rb
+++ backend/app/serializers/campaign_serializer.rb
@@ -1,5 +1,5 @@
 class CampaignSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :name, :tags, :user_id, :template_id, :created_at, :updated_at
+    attributes :name, :tags, :subject, :user_id, :template_id, :created_at, :updated_at
   end
   
