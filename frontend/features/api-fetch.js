bc288f7f6d2bd30ad62e044edc0650e0e8e16ae8
Xindi Xu <xindixu0@gmail.com>
2021-12-06 22:18:32 -0500

    some fixes

--- frontend/features/api-fetch.js
+++ frontend/features/api-fetch.js
@@ -3,11 +3,11 @@ const axios = require("axios")
 const apiFetch = ({ route, method = "get", token, params }) => {
   const headers = token ? { Authorization: `Bearer ${token}` } : {}
   return axios({
-    url: `http://localhost:3000/api/v1/${route}`,
+    url: `http://localhost:3000/api/v1${route}`,
     headers,
     data: params,
     method,
-  }).then(({ data: {data, status} }) => ({
+  }).then(({ data: { data, status } }) => ({
     data,
     status,
   }))
